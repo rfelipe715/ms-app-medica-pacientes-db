f084bd9820652cd5ee750775db0afc11f0f44051
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-06 09:52:45 -0400

    primer commit

---
+++ src/main/java/cl/duoc/ms_pacientes_db/MsPacientesDbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_pacientes_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPacientesDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPacientesDbApplication.class, args);
+	}
+
+}
