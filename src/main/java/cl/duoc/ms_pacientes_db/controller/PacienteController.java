2fb69dd73d82d39f3909ab2ea1973d330721fed1
Alejandra <alej.ramirezs@duocuc.cl>
2026-07-11 10:49:06 -0400

    refactor: desacopla la API de la entidad JPA, Controller y Service ahora trabajan con PacienteDto en vez de Paciente directamente

--- src/main/java/cl/duoc/ms_pacientes_db/controller/PacienteController.java
+++ src/main/java/cl/duoc/ms_pacientes_db/controller/PacienteController.java
@@ -1,6 +1,6 @@
 package cl.duoc.ms_pacientes_db.controller;
 
-import cl.duoc.ms_pacientes_db.model.entity.Paciente;
+import cl.duoc.ms_pacientes_db.model.dto.PacienteDto;
 import cl.duoc.ms_pacientes_db.service.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,23 +16,23 @@ public class PacienteController {
     @Autowired
     private PacienteService pacienteService;
 
-    // C - Create (Ya lo teníamos)
+    // C - Create
     @PostMapping
-    public ResponseEntity<Paciente> registrarPaciente(@RequestBody Paciente paciente) {
-        Paciente nuevoPaciente = pacienteService.registrarPaciente(paciente);
+    public ResponseEntity<PacienteDto> registrarPaciente(@RequestBody PacienteDto pacienteDto) {
+        PacienteDto nuevoPaciente = pacienteService.registrarPaciente(pacienteDto);
         return new ResponseEntity<>(nuevoPaciente, HttpStatus.CREATED);
     }
 
     // R - Read (Obtener todos los pacientes)
     @GetMapping
-    public ResponseEntity<List<Paciente>> obtenerTodos() {
-        List<Paciente> pacientes = pacienteService.obtenerTodos();
+    public ResponseEntity<List<PacienteDto>> obtenerTodos() {
+        List<PacienteDto> pacientes = pacienteService.obtenerTodos();
         return new ResponseEntity<>(pacientes, HttpStatus.OK);
     }
 
     // R - Read (Obtener un paciente por su ID)
     @GetMapping("/{id}")
-    public ResponseEntity<Paciente> obtenerPorId(@PathVariable Long id) {
+    public ResponseEntity<PacienteDto> obtenerPorId(@PathVariable Long id) {
         return pacienteService.obtenerPorId(id)
                 .map(paciente -> new ResponseEntity<>(paciente, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
@@ -40,9 +40,9 @@ public class PacienteController {
 
     // U - Update (Editar paciente existente)
     @PutMapping("/{id}")
-    public ResponseEntity<Paciente> editarPaciente(@PathVariable Long id, @RequestBody Paciente datosNuevos) {
+    public ResponseEntity<PacienteDto> editarPaciente(@PathVariable Long id, @RequestBody PacienteDto datosNuevos) {
         try {
-            Paciente pacienteActualizado = pacienteService.editarPaciente(id, datosNuevos);
+            PacienteDto pacienteActualizado = pacienteService.editarPaciente(id, datosNuevos);
             return new ResponseEntity<>(pacienteActualizado, HttpStatus.OK);
         } catch (RuntimeException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
